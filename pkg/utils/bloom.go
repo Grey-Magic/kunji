f3936d7c539483a87f818756f8f1f7806768ec2f
GreyMagic <145904155+Grey-Magic@users.noreply.github.com>
2026-05-04 21:09:49 +0530

    Add files via upload

--- pkg/utils/bloom.go
+++ pkg/utils/bloom.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"encoding/binary"
+	"fmt"
 	"hash/fnv"
+	"io"
 	"math"
+	"os"
 )
 
 type BloomFilter struct {
@@ -41,9 +45,78 @@ func (bf *BloomFilter) Test(data string) bool {
 	return true
 }
 
+func (bf *BloomFilter) Save(filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if err := binary.Write(file, binary.LittleEndian, bf.m); err != nil {
+		return err
+	}
+	if err := binary.Write(file, binary.LittleEndian, bf.k); err != nil {
+		return err
+	}
+	for _, b := range bf.bitset {
+		if err := binary.Write(file, binary.LittleEndian, b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func LoadBloomFilter(filePath string) (*BloomFilter, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var m, k uint64
+	if err := binary.Read(file, binary.LittleEndian, &m); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(file, binary.LittleEndian, &k); err != nil {
+		return nil, err
+	}
+
+	bitset := make([]uint64, m/64)
+	for i := range bitset {
+		if err := binary.Read(file, binary.LittleEndian, &bitset[i]); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+
+	return &BloomFilter{
+		bitset: bitset,
+		m:      m,
+		k:      k,
+	}, nil
+}
+
 func (bf *BloomFilter) hash(data string, seed uint64) uint64 {
 	h := fnv.New64a()
 	h.Write([]byte(data))
 	val := h.Sum64()
 	return val ^ (seed * 0xbf58476d1ce4e5b9)
+}
+
+func (bf *BloomFilter) Clear() {
+	for i := range bf.bitset {
+		bf.bitset[i] = 0
+	}
+}
+
+func (bf *BloomFilter) Merge(other *BloomFilter) error {
+	if bf.m != other.m || bf.k != other.k {
+		return fmt.Errorf("incompatible bloom filters")
+	}
+	for i := range bf.bitset {
+		bf.bitset[i] |= other.bitset[i]
+	}
+	return nil
 }
